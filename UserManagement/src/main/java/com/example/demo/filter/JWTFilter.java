aeb88b42751ad6522055999d361a5620b32ab19c
agarwalpriyanka4 <agarwalpriyanka5381@gmail.com>
2022-11-27 05:29:42 +0530

    UserManagement Draft1

---
+++ UserManagement/src/main/java/com/example/demo/filter/JWTFilter.java
@@ -0,0 +1,37 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JWTFilter extends GenericFilterBean{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpReq= (HttpServletRequest) request;
+		HttpServletResponse httpRes= (HttpServletResponse) response;
+		String authHeader=httpReq.getHeader("authorization");
+		
+		if(authHeader==null || !authHeader.startsWith("Bearer"))
+		{
+			throw new ServletException("Authorization failed due to invalid or missing header");
+		}
+		String jwtToken=authHeader.substring(7);
+		Claims claims =Jwts.parser().setSigningKey("Secret Key").parseClaimsJws(jwtToken).getBody();
+		
+		httpReq.setAttribute("username", claims);
+		
+	}
+
+}
---
+++ UserManagement/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.User;
+
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	@Query(value="Select u from User where u.username= :username and u.password= :password")
+	public User validateUser(String username, String password);
+}
